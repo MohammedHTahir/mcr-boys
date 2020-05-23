13e3a73962f396b87e5ee8e691b5ce87c40e2594
MohammedHTahir <tahir_mohammed@rocketmail.com>
2020-05-23 17:38:49 +0100

    deploying to github try last

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { DOCUMENT } from '@angular/common';
 
 import { ZoomMtg } from '@zoomus/websdk';
 
-
+ZoomMtg.setZoomJSLib('https://source.zoom.us/1.7.7/lib', '/av');
 ZoomMtg.preLoadWasm();
 ZoomMtg.prepareJssdk();
 
